974ee7d69493139cf3612f26cf5fc30e3679c429
Amirul <amirulislam9.e@gmail.com>
2025-11-12 14:11:11 +0600

    callback function practiced

---
+++ asynchronous_js/script.js
@@ -0,0 +1,61 @@
+// ==================tash one =======================
+let readTasbih = true;
+
+function startTasbih(callback) {
+  if (readTasbih === true) {
+    callback();
+  } else {
+    console.log("it is notstart");
+  }
+}
+
+function tasbihOne(callback) {
+  setTimeout(function () {
+    console.log("লা ইলাহা ইল্লালাহ");
+
+    callback();
+  }, 2000);
+}
+
+function tasbihTwo(callback) {
+  setTimeout(function () {
+    console.log("alhamdulillah");
+
+    callback();
+  }, 3000);
+}
+
+function tasbihthree() {
+  setTimeout(function () {
+    console.log("subahanallah");
+  }, 3000);
+}
+// startTasbih(function () {
+//   tasbihOne(function () {
+//     tasbihTwo(tasbihthree);
+//   });
+// });
+
+//  ===============task two ======================
+function taskStart(callback) {
+  console.log("task 1 started ");
+  callback();
+}
+
+function process(callback) {
+  setTimeout(function () {
+    console.log("processing... ");
+    callback();
+  }, 1000);
+}
+function complete(callback) {
+  setTimeout(function () {
+    console.log("Task 2 is complete ");
+  }, 2000);
+}
+
+// taskStart(function () {
+//   process(complete);
+// });
+
+//  ===============TASK THREE ======================
